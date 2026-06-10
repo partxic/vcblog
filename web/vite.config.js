6751d8363dd9e590156d1571e8a3140ca4b814c7
runoneall <runoobsteve@gmail.com>
2026-06-10 14:33:44 +0800

    feat: update dependencies and enhance Vite configuration
    - Added new dependencies: @vueuse/core, axios, element-plus
    - Updated devDependencies: unplugin-auto-import, unplugin-element-plus, unplugin-vue-components
    - Enhanced Vite configuration to include ElementPlus and auto-import features for improved component usage

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import ElementPlus from 'unplugin-element-plus/vite'
+
 import { fileURLToPath, URL } from 'node:url'
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        ElementPlus(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()]
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()]
+        })
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
