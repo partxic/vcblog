d02a0ed80b4db8531033e91836bf50e350746887
runoneall <runoobsteve@gmail.com>
2026-06-10 15:08:27 +0800

    feat: enhance App.vue with SiteInfoFetcher and router readiness handling; add initial Layout component

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,12 +1,27 @@
 <script setup>
-import { ref } from 'vue'
+document.title = '加载中...'
+
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
 import BackendValidator from '@/components/BackendValidator.vue'
+import SiteInfoFetcher from '@/components/SiteInfoFetcher.vue'
+import Layout from '@/components/Layout.vue'
 
 const backendOk = ref(false)
+const siteInfoFetched = ref(false)
+const routerReady = ref(false)
+
+onMounted(async () => {
+    const router = useRouter()
+    await router.isReady()
+    routerReady.value = true
+})
 </script>
 
 <template>
     <BackendValidator v-model="backendOk" />
+    <SiteInfoFetcher v-if="backendOk" v-model="siteInfoFetched" />
+    <Layout v-if="backendOk && siteInfoFetched && routerReady"></Layout>
 </template>
 
 <style scoped></style>
