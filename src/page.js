11dc1176f424e7b6b4d9549357fa92cadac8da7a
runoneall <runoobsteve@gmail.com>
2026-06-10 17:28:19 +0800

    feat: add content API endpoint to fetch page details by ID

--- src/page.js
+++ src/page.js
@@ -3,7 +3,7 @@ const page = express.Router()
 
 import storage from './storage.js'
 import { table } from './storage.js'
-import { eq } from 'drizzle-orm'
+import { and, eq } from 'drizzle-orm'
 
 page.get('/list', async (req, res) => {
     const result = await storage
@@ -15,6 +15,32 @@ page.get('/list', async (req, res) => {
         .where(eq(table.type, 'page'))
 
     return res.status(200).json(result)
+})
+
+page.get('/content', async (req, res) => {
+    const { id } = req.query
+    if (typeof id === 'undefined' || id === '') {
+        return res.status(400).send('请求错误')
+    }
+
+    const idNum = parseInt(id, 10)
+    if (isNaN(idNum)) {
+        return res.status(400).send('参数错误')
+    }
+
+    const result = await storage
+        .select({
+            title: table.title,
+            content: table.content
+        })
+        .from(table)
+        .where(and(eq(table.type, 'page'), eq(table.id, idNum)))
+
+    if (result.length === 0) {
+        return res.status(404).send('未找到')
+    }
+
+    return res.status(200).json(result[0])
 })
 
 export default page
