7441737d52dd41ddd0d1262dceab9a5f728b26a8
runoneall <runoobsteve@gmail.com>
2026-06-11 15:12:50 +0800

    feat: implement logout functionality and enhance tab click handling in Dashboard component

--- web/src/view/admin/Dashboard.vue
+++ web/src/view/admin/Dashboard.vue
@@ -3,6 +3,8 @@ document.title = '正在跳转...'
 
 import { useRoute, useRouter } from 'vue-router'
 import { computed, onMounted, watch } from 'vue'
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
 
 const route = useRoute()
 const router = useRouter()
@@ -12,6 +14,22 @@ const tabs = computed(() => {
     const dashboardRoute = allRoutes.find(route => route.name === 'dashboard')
     return dashboardRoute ? dashboardRoute.children || [] : []
 })
+
+const handleClick = (tab, _) => {
+    const name = tab.paneName
+    if (name !== 'logout') router.push({ name })
+    else doLogout()
+}
+
+const doLogout = async () => {
+    try {
+        const res = await axios.get('/api/auth/logout')
+        ElMessage.success(res.data)
+        router.push({ name: 'login' })
+    } catch (error) {
+        ElMessage.error(error.response.data)
+    }
+}
 
 const redirect = () => {
     if (!route.name || route.name === 'dashboard') router.push({ name: 'config-edit' })
@@ -29,8 +47,9 @@ onMounted(redirect)
 </script>
 
 <template>
-    <el-tabs :model-value="route.name" @tab-click="(tab, _) => router.push({ name: tab.paneName })">
+    <el-tabs :model-value="route.name" @tab-click="handleClick">
         <el-tab-pane v-for="tab in tabs" :label="tab.meta.tab" :name="tab.name" />
+        <el-tab-pane label="退出登录" name="logout" />
     </el-tabs>
     <router-view />
 </template>
