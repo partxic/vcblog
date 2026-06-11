661cf0bfd4b77de4880cb604e900dfde2c8fc350
runoneall <runoobsteve@gmail.com>
2026-06-11 14:44:11 +0800

    feat: implement redirect logic and document title in Dashboard component

--- web/src/view/admin/Dashboard.vue
+++ web/src/view/admin/Dashboard.vue
@@ -1,4 +1,26 @@
-<script setup></script>
+<script setup>
+document.title = '正在跳转...'
+
+import { useRoute, useRouter } from 'vue-router'
+import { onMounted, watch } from 'vue'
+
+const route = useRoute()
+const router = useRouter()
+
+const redirect = () => {
+    if (!route.name || route.name === 'dashboard') router.push({ name: 'config-edit' })
+    else router.push({ name: route.name })
+}
+
+watch(
+    () => route.name,
+    name => {
+        if (name === 'dashboard') redirect()
+    }
+)
+
+onMounted(redirect)
+</script>
 
 <template></template>
 
