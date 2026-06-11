25455309235bde815dcce5323e07b8badafb8b13
runoneall <runoobsteve@gmail.com>
2026-06-11 14:28:47 +0800

    feat: add dashboard child routes and create ConfigEdit, PageMgr, and PostMgr components

--- web/src/router.js
+++ web/src/router.js
@@ -37,7 +37,33 @@ export default createRouter({
                 {
                     path: 'dashboard',
                     name: 'dashboard',
-                    component: () => import('@/view/admin/Dashboard.vue')
+                    component: () => import('@/view/admin/Dashboard.vue'),
+                    children: [
+                        {
+                            path: 'config-edit',
+                            name: 'config-edit',
+                            meta: {
+                                tab: '配置编辑'
+                            },
+                            component: () => import('@/view/admin/dashboard/ConfigEdit.vue')
+                        },
+                        {
+                            path: 'page-mgr',
+                            name: 'page-mgr',
+                            meta: {
+                                tab: '页面管理'
+                            },
+                            component: () => import('@/view/admin/dashboard/PageMgr.vue')
+                        },
+                        {
+                            path: 'post-mgr',
+                            name: 'post-mgr',
+                            meta: {
+                                tab: '文章管理'
+                            },
+                            component: () => import('@/view/admin/dashboard/PostMgr.vue')
+                        }
+                    ]
                 }
             ]
         }
